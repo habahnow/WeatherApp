f6ee3c02eac282f77d02656464c3ae0e50267097
Allan Soto <Danielsoto03@gmail.com>
2025-04-04 19:21:27 -0700

    Fixed a naming issue with favorites when accessing localStorage

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -6,14 +6,14 @@ export const CityProvider = ( {children} ) => {
     const [favorites, setFavorites] = useState([]);
 
     useEffect(() => {
-        const storedFavs = localStorage.getItem("favorties");
-
+        const storedFavs = localStorage.getItem("favorites");
+        
         if (storedFavs) setFavorites(JSON.parse(storedFavs))
     }, [])
 
     useEffect( () => {
-        localStorage.setItem("favorites", JSON.stringify(favorites))
-    }, [favorites])
+                localStorage.setItem("favorites", JSON.stringify(favorites))
+            }, [favorites])
 
     const addToFavorites = (city) => {
         setFavorites( prev => [ ...prev, city ] );
