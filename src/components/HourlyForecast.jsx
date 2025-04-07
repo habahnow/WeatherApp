00e59088715df683f4319442c378dd36a9cba041
Allan Soto <Danielsoto03@gmail.com>
2025-04-06 22:49:46 -0700

    forgot to add commit

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,10 +1,13 @@
 function HourlyForecast ( {forecast} ){
+    console.log(forecast);
     return (
         <div className="hourly-forecast">
-            <p>Time: {forecast.dt_time}</p>
-            <p>Temperature:</p>
-            <p>percipitation:</p>
+            {/* <p>Object information: {forecast}</p> */}
+            <p>Time: {forecast.dt_txt} UTC</p>
+            <p>Temperature: {forecast.main.temp}</p>
+            <p>Percipitation: {forecast.pop} %</p>
         </div>
     )
     
 }
+export default HourlyForecast;
